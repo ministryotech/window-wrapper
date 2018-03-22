a58a00a8be6fbda3b84324adbcd76e4f06dc1588
Keith Jackson <keith@ministryotech.co.uk>
2018-03-22 17:40:20 +0000

    Fixed dependencies

--- deploy/deploy-npm.bat
+++ deploy/deploy-npm.bat
@@ -3,10 +3,13 @@
 REM -- Uncomment to Init NPM first run --
 REM CALL ..\..\set-npm.bat
 
-ECHO Compiling JS...
 CD ..\
+
+ECHO Linting...
+CALL .\node_modules\.bin\eslint .\path-manager.js
+
+ECHO Compiling JS...
 CALL gulp
 
 ECHO Publishing to NPM...
 CALL npm publish deploy\build\
-pause
