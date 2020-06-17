b682bcd78f138f7b73c42fa3faf43a17c322ae48
Keith Jackson <keith.jackson@parityprojects.com>
2020-06-17 13:13:49 +0100

    Node modules issue fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
-const gulp = require("gulp")
-const clean = require("gulp-clean")
-const rename = require("gulp-rename")
+const gulp = require('gulp')
+const clean = require('gulp-clean')
+const rename = require('gulp-rename')
 
 const paths = {
-    scriptsBuildPath: "deploy/build/"
+    scriptsBuildPath: 'deploy/build/'
 }
 
-gulp.task("clean", function() {
+gulp.task('clean', function() {
     return gulp.src(paths.scriptsBuildPath, { read: false })
         .pipe(clean({ force: true }))
 })
 
-gulp.task("build", function() {
-    gulp.src("window-wrapper.js")
-        .pipe(rename("index.js"))
+gulp.task('build', function() {
+    gulp.src('window-wrapper.js')
+        .pipe(rename('index.js'))
         .pipe(gulp.dest(paths.scriptsBuildPath))
-    gulp.src("readme.md")
+    gulp.src('readme.md')
         .pipe(gulp.dest(paths.scriptsBuildPath))
-    return gulp.src("package.json")
+    return gulp.src('package.json')
         .pipe(gulp.dest(paths.scriptsBuildPath))
 })
 
-gulp.task("default", ["build"])
+gulp.task('default', ['build'])
