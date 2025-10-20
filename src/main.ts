45a33f2b38382fda9310ed156531b49c1fc1af73
Keith Jackson <temporal-net@live.co.uk>
2025-10-20 22:59:33 +0100

    TypeScript Test Harness works and links!

--- src/main.ts
+++ src/main.ts
@@ -34,23 +34,23 @@ const WindowWrapper = {
      * The options argument sets listener-specific options. For compatibility this can be a boolean, in which case the method
      * behaves exactly as if the value was specified as options capture.
      *
-     * When set to true, options capture prevents callback from being invoked when the event's eventPhase attribute value is
-     * BUBBLING_PHASE. When false (or not present), callback will not be invoked when event's eventPhase attribute value is
-     * CAPTURING_PHASE. Either way, callback will be invoked if event's eventPhase attribute value is AT_TARGET.
+     * When set to true, options capture prevents callback from being invoked when the events eventPhase attribute value is
+     * BUBBLING_PHASE. When false (or not present), callback will not be invoked when events eventPhase attribute value is
+     * CAPTURING_PHASE. Either way, callback will be invoked if events eventPhase attribute value is AT_TARGET.
      *
      * When set to true, options passive indicates that the callback will not cancel the event by invoking preventDefault().
      * This is used to enable performance optimizations described in § 2.8 Observing event listeners.
      *
-     * When set to true, options once indicates that the callback will only be invoked once after which the event listener
+     * When set to true, options once indicates that the callback will only be invoked once, after which the event listener
      * will be removed.
      *
      * The event listener is appended to target's event listener list and is not appended if it has the same type, callback,
      * and capture.
      * @param type The event type.
      * @param listener The event listener.
-     * @param options
+     * @param [options]
      */
-    addEventListener: function(type: string, listener: EventListenerOrEventListenerObject, options: boolean | AddEventListenerOptions) {
+    addEventListener: function(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions) {
         return window.addEventListener(type, listener, options)
     },
 
@@ -58,9 +58,9 @@ const WindowWrapper = {
      * Removes the event listener in target's event listener list with the same type, callback, and options.
      * @param type The event type.
      * @param listener The event listener.
-     * @param options
+     * @param [options]
      */
-    removeEventListener: function(type: string, listener: EventListenerOrEventListenerObject, options: boolean | EventListenerOptions) {
+    removeEventListener: function(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions) {
         return window.removeEventListener(type, listener, options)
     },
 }
--- test-harness-js/src/main.js
+++ test-harness-js/src/main.js
@@ -1,24 +1,41 @@
 import './style.css'
+
 import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import WindowWrapper from 'window-wrapper'
 
-setupCounter(document.querySelector('#counter'))
+document.querySelector('#jsLogo').src = javascriptLogo
+
+document.querySelector('#currentUri').innerText = WindowWrapper.getHref()
+document.querySelector('#hostName').innerText = WindowWrapper.getHost()
+document.querySelector('#path').innerText = WindowWrapper.getPath()
+
+/**
+ * Clickable page event.
+ * @param {Event} event
+ */
+const clickablePageEvent = (event) => {
+    event.preventDefault()
+    //TS const target = event.target as HTMLElement
+    // noinspection JSUnresolvedReference
+    const targetId = event.target.getAttribute('id')
+    if (targetId === 'eventOnButton' || targetId === 'eventOffButton') return
+    document.querySelector('#eventTestButtonResult').innerText = "You clicked the page!"
+}
+
+document.querySelector('#eventOnButton').addEventListener('click', (event) => {
+    event.preventDefault()
+    document.querySelector('#eventOffButton').removeAttribute('disabled')
+    document.querySelector('#eventOnButton').setAttribute('disabled', 'disabled')
+
+    WindowWrapper.addEventListener('click', clickablePageEvent)
+    document.querySelector('#eventTestButtonResult').innerText = "Events are switched on!"
+})
+
+document.querySelector('#eventOffButton').addEventListener('click', (event) => {
+    event.preventDefault()
+    document.querySelector('#eventOnButton').removeAttribute('disabled')
+    document.querySelector('#eventOffButton').setAttribute('disabled', 'disabled')
+    WindowWrapper.removeEventListener('click', clickablePageEvent)
+    document.querySelector('#eventTestButtonResult').innerText = "Clicking won't do anything now :("
+})
